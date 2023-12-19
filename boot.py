b32b57300ab745debed0068591a8f4a196edb4b2
Stavros <steveiliop56@gmail.com>
2023-12-19 15:14:43 +0200

    fix(boot): revert back to pin based checking

--- boot.py
+++ boot.py
@@ -1,10 +1,9 @@
 # License : GPLv2.0
 # copyright (c) 2023  Dave Bailey
 # Author: Dave Bailey (dbisu, @daveisu)
-# Wio terminal addition: Stavros (steveiliop56, @steveiliop56)
+# Wio terminal addition: Steve Iliopoulos (steveiliop56, @steveiliop56)
 
 from board import *
-from duckyinpython import getStorageStatus, getScreenStatus
 import board
 import digitalio
 import storage
@@ -12,25 +11,36 @@ import displayio
 import os
 import sys
 
-storageStatus = False
-storageStatus = getStorageStatus()
+noStoragePin = digitalio.DigitalInOut(board.D0)
+noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
+noStorageStatus = noStoragePin.value
 
-if storageStatus == True:
+# If D0 is not connected, it will default to being pulled high (True)
+# If D0 is connected to GND, it will be low (False)
+
+if noStorageStatus == True:
+    # don't show USB drive to host PC
     storage.disable_usb_drive()
     print("Disabling USB drive.")
 else:
     # normal boot
     print("USB drive enabled.")
 
-screenStatus = False
-screenStatus = getScreenStatus()
+noScreenPin = digitalio.DigitalInOut(board.D1)
+noScreenPin.switch_to_input(pull=digitalio.Pull.DOWN)
+noScreenStatus = noScreenPin.value
 
-if screenStatus == True:
+# If D1 is not connected, it will default to being pulled high (False)
+# If D1 is connected to 5V, it will be low (True)
+
+if noScreenStatus == True:
+    # don't enable the display output
     displayio.release_displays()
     print("Disabling the screen.")
 else:
     # normal boot
     print("Screen enabled.")
+
 
 # If the middle switch is not connected, it will default to being pulled high (False)
 # If the middle switch is pressed, it will be low (True)
